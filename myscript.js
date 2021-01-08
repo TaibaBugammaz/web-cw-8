6fca61df9112200613fd761ecc5b1279af99d813
TaibaBugammaz <76516938+TaibaBugammaz@users.noreply.github.com>
2021-01-08 11:46:01 +0300

    1

--- myscript.js
+++ myscript.js
@@ -5,10 +5,33 @@ let board = [
 ]
 
 
+let turn= "O";
+
 
 function press(cellNO) {
+    
+    if (turn == "O") {
+        turn = "X";
+    }else{
+        turn = "O";
+    }
+
+
+    board[cellNO] = turn;
+    let board = document.getElementById(cellNO);
+    board.innerText= turn;
+   
+
+    if (board[0][0] == turn && board[0][1] == turn && board[0][2] == turn) {
+        document.getElementById("status").innerText=`BRAVOOOO! ${turn}`;
+    }
+    if (board[0][0] == turn && board[1][1] == turn && board[2][2] == turn) {
+        document.getElementById("status").innerText=`BRAVOOOO! ${turn}`;
+    }
+}
+
+
 
     // Do stuff
     console.log(cellNO)
     
-}
